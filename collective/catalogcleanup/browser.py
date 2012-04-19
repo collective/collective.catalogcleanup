33edfd059238f33027202a02f97bd1d0d05ee3ed
Maurits van Rees <maurits@vanrees.org>
2012-04-19 16:09:20 +0200

    typo

--- collective/catalogcleanup/browser.py
+++ collective/catalogcleanup/browser.py
@@ -1,7 +1,7 @@
 from Products.Five import BrowserView
 
 
-class Patch(BrowserView):
+class Cleanup(BrowserView):
 
     def __call__(self):
         return u"TODO: collective.catalogcleanup not functioning yet."
