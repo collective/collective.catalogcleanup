3deeb90a40c6232f7a6b36c621fca227a756e20d
Maurits van Rees <maurits@vanrees.org>
2012-04-25 21:41:53 +0200

    Handle reference_catalog too.
    Do the normal checks and also remove brains for which the source or target object can not be reached.

--- collective/catalogcleanup/browser.py
+++ collective/catalogcleanup/browser.py
@@ -15,12 +15,14 @@ class Cleanup(BrowserView):
     def __call__(self):
         self.messages = []
         self.msg("Starting catalog cleanup.")
-        catalog_ids = ['portal_catalog', 'uid_catalog']
+        catalog_ids = ['portal_catalog', 'uid_catalog', 'reference_catalog']
         for catalog_id in catalog_ids:
             self.msg("Handling catalog %s." % catalog_id)
             self.report(catalog_id)
             self.remove_without_uids(catalog_id)
             self.remove_without_object(catalog_id)
+        if 'reference_catalog' in catalog_ids:
+            self.check_references()
         self.msg("Done with catalog cleanup.")
         return '\n'.join(self.messages)
 
@@ -85,9 +87,39 @@ class Cleanup(BrowserView):
             self.msg("Removed %d brains from %s with status %s." % (
                 value, catalog_id, error))
 
-    def get_object_or_status(self, brain):
+    def check_references(self, catalog_id='reference_catalog'):
+        """Remove all brains without proper references.
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, catalog_id)
+        status = {}
+        standard_filter = {'path': '/'}
+        brains = list(catalog(**standard_filter))
+        getters = ('getSourceObject', 'getTargetObject')
+        for brain in brains:
+            # This should always work, as cleanup should already have
+            # been done:
+            ref = brain.getObject()
+            for getter in getters:
+                obj = self.get_object_or_status(ref, getter)
+                if not isinstance(obj, basestring):
+                    continue
+                # We have an error.  Remove the reference brain.
+                count = status.get(obj, 0)
+                status[obj] = count + 1
+                catalog.uncatalog_object(brain.getPath())
+                # No need for the second getter if the first already
+                # fails.
+                break
+
+        for error, value in status.items():
+            self.msg("Removed %d brains from %s with status %s." % (
+                value, catalog_id, error))
+
+    def get_object_or_status(self, brain, getter='getObject'):
         try:
-            obj = brain.getObject()
+            # Usually: brain.getObject()
+            obj = getattr(brain, getter)()
         except (ConflictError, KeyboardInterrupt):
             raise
         except (NotFound, AttributeError):
