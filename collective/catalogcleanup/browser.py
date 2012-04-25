be0ab65c2e5f8b7434d89bebc70906339af4a65a
Maurits van Rees <maurits@vanrees.org>
2012-04-25 17:10:55 +0200

    restructure, handling one catalog at a time.

--- collective/catalogcleanup/browser.py
+++ collective/catalogcleanup/browser.py
@@ -11,33 +11,38 @@ class Cleanup(BrowserView):
 
     def __call__(self):
         self.messages = []
-        context = aq_inner(self.context)
-        pc = getToolByName(context, 'portal_catalog')
-        uid_cat = getToolByName(context, 'uid_catalog')
         self.msg("Starting catalog cleanup.")
-        self.msg("portal_catalog brains: %d" % len(pc))
-        self.msg("uid_catalog brains: %d" % len(uid_cat))
-        # Using an empty filter to query the catalog will give a
-        # DeprecationWarning and may not work on Zope 2.14 anymore.
-        # We try to avoid this.
-        standard_filter = {'path': '/'}
-        self.msg("portal_catalog brains: %d" % len(pc(**standard_filter)))
-        # Actually, the uid_catalog usually has no path index, so we
-        # get the DeprecationWarning anyway.  So be it.
-        self.msg("uid_catalog brains: %d" % len(uid_cat(**standard_filter)))
-        uncatalog = 0
+        context = aq_inner(self.context)
+        catalog_ids = ['portal_catalog', 'uid_catalog']
+        for catalog_id in catalog_ids:
+            self.msg("Handling catalog %s." % catalog_id)
+            catalog = getToolByName(context, catalog_id)
+            size = len(catalog)
+            self.msg("Brains in %s: %d" % (catalog_id, size))
+            # Getting all brains from the catalog may give a different
+            # result for some reason.  Using an empty filter to query
+            # the catalog will give a DeprecationWarning and may not
+            # work on Zope 2.14 anymore.  We try to avoid this.
+            standard_filter = {'path': '/'}
+            # Actually, the uid_catalog usually has no path index, so
+            # we get the DeprecationWarning anyway.  So be it.
+            alternative_size = len(catalog(**standard_filter))
+            if alternative_size != size:
+                self.msg("Brains in %s using standard filter is different: %d"
+                         % (catalog_id, alternative_size), level=logging.WARN)
+            uncatalog = 0
 
-        # Remove all brains without UID.
-        for catalog in pc, uid_cat:
+            # Remove all brains without UID.
             uid_filter = {'UID': None}
             # We need to get the complete list instead of a lazy
             # mapping, otherwise iterating misses half of the brains
-            # and we would need to try it again.
+            # and we would need to try again.
             brains = list(catalog(**uid_filter))
             for brain in brains:
                 catalog.uncatalog_object(brain.getPath())
                 uncatalog += 1
-        self.msg("Removed %d catalog brains without UID." % uncatalog)
+            self.msg("Removed %d brains without UID from %s." % (
+                uncatalog, catalog_id))
 
         return '\n'.join(self.messages)
 
