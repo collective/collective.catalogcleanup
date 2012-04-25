1cbef69e750993426cf00af6be35d29e4239c88c
Maurits van Rees <maurits@vanrees.org>
2012-04-25 18:07:52 +0200

    Restructure, adding methods report and remove_without_uids.

--- collective/catalogcleanup/browser.py
+++ collective/catalogcleanup/browser.py
@@ -12,40 +12,50 @@ class Cleanup(BrowserView):
     def __call__(self):
         self.messages = []
         self.msg("Starting catalog cleanup.")
-        context = aq_inner(self.context)
         catalog_ids = ['portal_catalog', 'uid_catalog']
         for catalog_id in catalog_ids:
             self.msg("Handling catalog %s." % catalog_id)
-            catalog = getToolByName(context, catalog_id)
-            size = len(catalog)
-            self.msg("Brains in %s: %d" % (catalog_id, size))
-            # Getting all brains from the catalog may give a different
-            # result for some reason.  Using an empty filter to query
-            # the catalog will give a DeprecationWarning and may not
-            # work on Zope 2.14 anymore.  We try to avoid this.
-            standard_filter = {'path': '/'}
-            # Actually, the uid_catalog usually has no path index, so
-            # we get the DeprecationWarning anyway.  So be it.
-            alternative_size = len(catalog(**standard_filter))
-            if alternative_size != size:
-                self.msg("Brains in %s using standard filter is different: %d"
-                         % (catalog_id, alternative_size), level=logging.WARN)
-            uncatalog = 0
-
-            # Remove all brains without UID.
-            uid_filter = {'UID': None}
-            # We need to get the complete list instead of a lazy
-            # mapping, otherwise iterating misses half of the brains
-            # and we would need to try again.
-            brains = list(catalog(**uid_filter))
-            for brain in brains:
-                catalog.uncatalog_object(brain.getPath())
-                uncatalog += 1
-            self.msg("Removed %d brains without UID from %s." % (
-                uncatalog, catalog_id))
-
+            self.report(catalog_id)
+            self.remove_without_uids(catalog_id)
+        self.msg("Done with catalog cleanup.")
         return '\n'.join(self.messages)
 
     def msg(self, msg, level=logging.INFO):
         logger.log(level, msg)
         self.messages.append(msg)
+
+    def report(self, catalog_id):
+        """Report about this catalog.
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, catalog_id)
+        size = len(catalog)
+        self.msg("Brains in %s: %d" % (catalog_id, size))
+        # Getting all brains from the catalog may give a different
+        # result for some reason.  Using an empty filter to query
+        # the catalog will give a DeprecationWarning and may not
+        # work on Zope 2.14 anymore.  We try to avoid this.
+        standard_filter = {'path': '/'}
+        # Actually, the uid_catalog usually has no path index, so
+        # we get the DeprecationWarning anyway.  So be it.
+        alternative_size = len(catalog(**standard_filter))
+        if alternative_size != size:
+            self.msg("Brains in %s using standard filter is different: %d"
+                     % (catalog_id, alternative_size), level=logging.WARN)
+
+    def remove_without_uids(self, catalog_id):
+        """Remove all brains without UID.
+        """
+        context = aq_inner(self.context)
+        catalog = getToolByName(context, catalog_id)
+        uncatalog = 0
+        uid_filter = {'UID': None}
+        # We need to get the complete list instead of a lazy
+        # mapping, otherwise iterating misses half of the brains
+        # and we would need to try again.
+        brains = list(catalog(**uid_filter))
+        for brain in brains:
+            catalog.uncatalog_object(brain.getPath())
+            uncatalog += 1
+        self.msg("Removed %d brains without UID from %s." % (
+            uncatalog, catalog_id))
