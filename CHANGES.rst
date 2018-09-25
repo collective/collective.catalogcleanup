3c83efb5d3c78da5341a019b57ee6fdb6dd9755c
Maurits van Rees <maurits@vanrees.org>
2018-09-25 22:42:46 +0200

    Bumped version for feature release.
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.8.1 (unreleased)
+1.9.0 (unreleased)
 ------------------
 
 - Catch TypeError when getting object for brain.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.8.1.dev0'
+version = '1.9.0.dev0'
 
 setup(name='collective.catalogcleanup',
       version=version,
