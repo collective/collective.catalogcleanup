b75e1b3614c0b8218ddf3099353595ad4ebde582
Maurits van Rees <maurits@vanrees.org>
2020-02-08 17:48:30 +0100

    Back to development: 1.10.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.10.0'
+version = '1.10.1.dev0'
 
 setup(name='collective.catalogcleanup',
       version=version,
